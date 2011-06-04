2385aee6cc3cbf7be8e3031608bcc2c2d295eb13
Tsuyoshi MAEHANA <sandinista21@gmail.com>
2011-06-04 15:19:20 +0900

    book の initialize のテストグリーン！

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,4 +1,30 @@
-# -*- condig: utf8 -*-
+# -*- coding: utf-8 -*-
 $:.unshift File.dirname(__FILE__)
 require 'spec_helper'
 require 'book'
+require 'circulation_status'
+
+describe Book do
+  describe "インスタンス化されたとき" do
+    before do
+      @book = Book.new
+    end
+    subject {@book}
+    it do
+      subject.id.should == "0"
+    end
+    it do
+      subject.title.should == ""
+    end
+    it do
+      subject.author.should == ""
+    end
+    it do
+      subject.isbn.should == ""
+    end
+    it do
+      subject.status.should == CirculationStatus::STOCKED
+    end
+  end
+end
+
