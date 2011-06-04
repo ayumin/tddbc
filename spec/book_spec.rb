fabf60dfdb537cbdcc09da5fc6b979af952f965d
Tsuyoshi MAEHANA <sandinista21@gmail.com>
2011-06-04 16:51:24 +0900

    bookインスタンスの比較テストコードを追加

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -29,5 +29,40 @@ describe Book do
       it { should == CirculationStatus::STOCKED }
     end
   end
+
+  describe "bookの属性値が等しければeqlである" do
+    before do
+      @book1 = Book.new
+      @book2 = Book.new
+    end
+    context "empty book is equal" do
+      it {@book1.eql?(@book2).should be_true}
+    end
+    context "has same value book is equal" do
+      it {
+        [@book1,  @book2].each{ |b|
+          b.id = "010"
+          b.title = "tddbc"
+          b.author = "ayumin"
+          b.isbn = "1234567890123"
+          b.status = CirculationStatus::LENDING
+        }
+        @book1.eql?(@book2).should be_true
+      }
+    end
+    context "has not same value book is not equal" do
+      it {
+        [@book1,  @book2].each{ |b|
+          b.id = "010"
+          b.title = "tddbc"
+          b.author = "ayumin"
+          b.isbn = "1234567890123"
+          b.status = CirculationStatus::LENDING
+        }
+        @book2.title = "tdddc"
+        @book1.eql?(@book2).should be_false
+      }
+    end
+  end
 end
 
