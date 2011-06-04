bc770db0f5d9c6a8d3449137280ef3b281250823
Tsuyoshi MAEHANA <sandinista21@gmail.com>
2011-06-04 16:30:02 +0900

    database の add のテストを追加した

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,6 +2,7 @@
 $:.unshift File.dirname(__FILE__)
 require 'spec_helper'
 require 'database'
+require 'book'
 
 describe Database do
   describe "インスタンス化されたとき" do
@@ -30,5 +31,23 @@ describe Database do
 #      end
 #    end
   end
+
+  describe "bookをaddした場合" do
+    before do
+      @database = Database.new "book.bin"
+    end
+    subject {@database}
+    describe "findで取得した book と同一である" do
+      it do
+        book = Book.new
+        book.id = "10"
+
+        subject.add(book)
+        found_book = subject.find(book.id)
+        found_book.eql?(book).should be_true
+      end
+    end
+  end
+
 end
 
